a6ec8aa6fde2c7f4f4cfb4c5e9deb601df39dfe2
Bartosz Gajda <bg@bartoszgajda.com>
2018-02-04 21:34:02 +0000

    Added Firebase init keys.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
-import { MyApp } from './app.component';
+import { App } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {LoginPage} from "../pages/login/login";
+import {RegisterPage} from "../pages/register/register";
+import {AngularFireModule} from "angularfire2";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDr4odv1_woIeygBZMBAN6vP8knRA60s4",
+  authDomain: "notarin2-2428c.firebaseapp.com",
+  databaseURL: "https://notarin2-2428c.firebaseio.com",
+  projectId: "notarin2-2428c",
+  storageBucket: "notarin2-2428c.appspot.com",
+  messagingSenderId: "112925218288"
+};
+
+const PAGES = [
+  App,
+  HomePage,
+  LoginPage,
+  RegisterPage
+];
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: PAGES,
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(App),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: PAGES,
   providers: [
     StatusBar,
     SplashScreen,
