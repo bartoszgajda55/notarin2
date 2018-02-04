648df0419b44522b6086e3bf3d7531b48b30639a
Bartosz Gajda <bg@bartoszgajda.com>
2018-02-04 21:59:46 +0000

    Added basic register page.

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the RegisterPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage } from 'ionic-angular';
+import {NgForm} from "@angular/forms";
 
 @IonicPage()
 @Component({
@@ -15,11 +9,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegisterPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
+  onRegister(form: NgForm): void {
+    console.log(form.value);
   }
 
 }
