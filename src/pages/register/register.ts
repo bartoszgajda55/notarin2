563756d5501381015c4ed2e744e82608efc3f62d
Bartosz Gajda <bg@bartoszgajda.com>
2018-02-10 21:40:55 +0000

    Added loader and toast in Register Page to show error codes in nice way.

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {IonicPage, NavController} from 'ionic-angular';
+import {IonicPage, LoadingController, NavController, ToastController} from 'ionic-angular';
 import {NgForm} from "@angular/forms";
 import {AuthProvider} from "../../providers/auth/auth";
 import {MenuPage} from "../menu/menu";
@@ -13,17 +13,38 @@ export class RegisterPage {
 
   constructor(
     private authProvider: AuthProvider,
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController
   ) { }
 
   onRegister(form: NgForm): void {
+    if (form.value.password1 !== form.value.password2) {
+      let toast = this.toastCtrl.create({
+        message: "Passwords don't match",
+        duration: 3000,
+        position: 'top'
+      });
+      toast.present();
+      return null;
+    }
+    let loading = this.loadingCtrl.create({
+      content: 'Registering....',
+      dismissOnPageChange: true
+    });
+    loading.present();
     this.authProvider.registerWithEmailAndPassword(form.value.email, form.value.password1)
       .then(value => {
-        console.log(value);
         this.navCtrl.setRoot(MenuPage);
       })
       .catch(reason => {
-        console.log(reason);
+        loading.dismiss();
+        let toast = this.toastCtrl.create({
+          message: reason.message,
+          duration: 3000,
+          position: 'top'
+        });
+        toast.present();
       });
   }
 }
